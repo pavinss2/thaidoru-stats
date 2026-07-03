934941ec45c290c09332faa8c0bc58f77413668e
Pavin P <pavin.ss2@gmail.com>
2026-07-04 01:32:37 +0700

    Format line graph and bar graph y-axis ticks to show actual full numbers

--- app.js
+++ app.js
@@ -977,9 +977,7 @@ function renderGrowthChart() {
                         color: '#8E8E9F',
                         font: { family: 'Outfit' },
                         callback: function(value) {
-                            if (value >= 1e6) return (value / 1e6).toFixed(2) + 'M';
-                            if (value >= 1e3) return (value / 1e3).toFixed(2) + 'k';
-                            return value;
+                            return new Intl.NumberFormat().format(value);
                         }
                     }
                 }
